369d70bf67f09b249d3ae34cdd3312404e8bb991
Ahn Soohan <ahn-soohan@fout.jp>
2018-05-08 00:14:46 +0900

    [Lecture 5] Import and use former package.

--- lecture5/main.go
+++ lecture5/main.go
@@ -2,8 +2,7 @@ package main
 
 import (
 	"fmt"
-	"golang.org/x/net/html"
-	"io"
+	"github.com/gophercises/lecture4/parselinks"
 	"net/http"
 	"net/url"
 	"os"
@@ -16,54 +15,8 @@ type QueueData struct {
 	Path  string
 }
 
-type Link struct {
-	Href string
-	Text string
-}
-
 var maxDepth = 3
 var queueSize = 100
-
-func RetrieveAllLinks(f io.ReadCloser) []Link {
-	z := html.NewTokenizer(f)
-	var result []Link
-	for {
-		t := z.Next()
-		if t == html.ErrorToken {
-			break
-		}
-		tk := z.Token()
-
-		if tk.Data == "a" {
-			var url string
-			for _, v := range tk.Attr {
-				if v.Key == "href" {
-					url = v.Val
-				}
-			}
-			l := Link{
-				Href: url,
-			}
-			for {
-				t := z.Next()
-				if t == html.ErrorToken {
-					break
-				}
-				tk := z.Token()
-				if t == html.EndTagToken && tk.Data == "a" {
-					break
-				}
-				if t == html.TextToken {
-					l.Text += strings.Trim(tk.Data, "\n ")
-				}
-			}
-
-			result = append(result, l)
-		}
-	}
-
-	return result
-}
 
 func retrieve(c chan QueueData, hostname string) map[string]int {
 	memo := make(map[string]int)
@@ -84,10 +37,12 @@ func retrieve(c chan QueueData, hostname string) map[string]int {
 				panic(err)
 			}
 			defer res.Body.Close()
-			links := RetrieveAllLinks(res.Body)
+			links := parselinks.RetrieveAllLinks(res.Body)
 
 			for i, _ := range links {
-				if matched, err := regexp.Match(hostname, []byte(links[i].Href)); matched == true && err == nil {
+				if matched, err := regexp.Match(hostname, []byte(links[i].Href)); err != nil {
+					panic(err)
+				} else if matched {
 					// Trim Hostname.
 					path := strings.TrimPrefix(links[i].Href, hostname)
 					fmt.Printf("key: %s, Link: %s\n", links[i].Href, path)
@@ -101,8 +56,6 @@ func retrieve(c chan QueueData, hostname string) map[string]int {
 						}
 						c <- nextData
 					}
-				} else if err != nil {
-					panic(err)
 				}
 			}
 		default:
