f96307698c0f12700709ec71d9ce8af310862659
Ahn Soohan <ahn-soohan@fout.jp>
2018-05-16 00:21:18 +0900

    [Lecture 9] Preparing..

---
+++ lecture9/deck/deck.go
@@ -0,0 +1,83 @@
+package deck
+
+import (
+	"fmt"
+	"sort"
+	"strconv"
+)
+
+type Card struct {
+	Suit    string
+	Numeric string
+}
+
+var SuitMap = map[string]int{
+	"Spade":   1,
+	"Diamond": 2,
+	"Club":    3,
+	"Heart":   4,
+}
+
+var NumericMap = map[string]int{
+	"A": 1,
+	"J": 11,
+	"Q": 12,
+	"K": 13,
+}
+
+type Deck []Card
+
+func (d Deck) Len() int      { return len(d) }
+func (d Deck) Swap(i, j int) { d[i], d[j] = d[j], d[i] }
+func (d Deck) Less(i, j int) bool {
+	iSuit := SuitMap[d[i].Suit]
+	jSuit := SuitMap[d[j].Suit]
+	if iSuit != jSuit {
+		return iSuit < jSuit
+	}
+
+	iNumeric, err := strconv.Atoi(d[i].Numeric)
+	if err != nil {
+		iNumeric = NumericMap[d[i].Numeric]
+	}
+	jNumeric, err := strconv.Atoi(d[j].Numeric)
+	if err != nil {
+		jNumeric = NumericMap[d[j].Numeric]
+	}
+	return iNumeric < jNumeric
+}
+
+func (d Deck) AllCards() {
+	for i, _ := range d {
+		fmt.Printf("%s %s\n", d[i].Suit, d[i].Numeric)
+	}
+}
+
+func New() Deck {
+	var cards Deck
+	for suit, _ := range SuitMap {
+		for n, _ := range NumericMap {
+			card := Card{
+				Suit:    suit,
+				Numeric: n,
+			}
+			cards = append(cards, card)
+		}
+		for i := 2; i <= 10; i++ {
+			n := strconv.Itoa(i)
+			card := Card{
+				Suit:    suit,
+				Numeric: n,
+			}
+			cards = append(cards, card)
+		}
+	}
+
+	sort.Sort(cards)
+	return cards
+}
+
+func main() {
+	cards := New()
+	cards.AllCards()
+}
---
+++ lecture9/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/gophercises/lecture9/deck"
+)
+
+func main() {
+	cards := deck.New()
+	cards.AllCards()
+}
